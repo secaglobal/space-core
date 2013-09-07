b2fb669f33365bfdc28e1d27371ad73b9f47079a
lev <lev@corp>
2013-09-07 12:39:13 +0300

    fix

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = {
     Router: require('./lib/router'),
     Exception: require('./lib/exception'),
     WebAPI: require('./lib/web-api'),
+    Util: require('./lib/util'),
     Ext: {
         AccessControl: require('./lib/ext/access-control')
     }
---
+++ lib/util.coffee
@@ -0,0 +1,4 @@
+module.exports = {}
+
+module.exports.getServerTime () ->
+    new Date().getTime() / 1000
